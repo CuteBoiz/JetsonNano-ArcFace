d852529d26df8110a95b90979cc923506777cdf8
CuteBoiz <16521619@gm.uit.edu.vn>
2020-12-19 04:07:23 +0700

    change to predict

--- CMakeFiles/main_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/main_autogen"
-  "main_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/main_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
