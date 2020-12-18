04c2615ff3acdc8db3751bdc819b292d9dbbaf34
CuteBoiz <16521619@gm.uit.edu.vn>
2020-12-19 04:38:35 +0700

    add_ID + predict executed!

---
+++ src/main_addID.cpp
@@ -0,0 +1,98 @@
+#include "mtcnn.h"
+#include <iostream>
+#include <ctime>
+#include <fstream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#define PI  3.141592653589793238463
+#define NORF_LINES 256
+#define MAX_IMGS 100
+string SAVE_DIR = "../Dataset/";
+int nrof_imgs = 1;
+string IDName = "Phat";
+
+int main(){
+	Mat frame;
+    VideoCapture cap(0);
+    if(!cap.isOpened()){
+        cout<<"Fail to open video!"<<endl;
+        return -1;
+    }
+    clock_t start;
+    cap>>frame;
+    if(!frame.data) {
+        cout << "unable to open camera" << endl;
+        return -1;
+    }
+    mtcnn detector(frame.rows, frame.cols);
+
+    while(1){
+        start = clock();
+        cap >> frame;
+        if (frame.empty()) break;
+
+        int h = frame.rows;
+		int w = frame.cols;
+		int min_side = h < w ? h : w;
+		int radius = min_side / 2 - 20;
+		circle(frame, Point((int)w/2, (int)h/2), min_side, Scalar(0, 0, 0), min_side+70);
+
+		for (int i = 0; i < NORF_LINES; i++) {
+			float angle = 2 * PI * i / NORF_LINES;
+			float x = (int)w/2 + sin(angle) * radius;
+			float y = (int)h/2 + cos(angle) * radius;
+			line(frame, Point(x, y), Point(x+sin(angle)*10, y+cos(angle)*10), Scalar(255, 255, 255), 1);
+		}
+
+		double percent = (double)nrof_imgs/(double)MAX_IMGS;
+		int nrof_big_lines = NORF_LINES * percent;
+
+		for (int j = 0; j < nrof_big_lines; j++){
+			float angle = 2 * PI * j / NORF_LINES;
+			float x = (int)w/2 + sin(angle) * radius;
+			float y = (int)h/2 + cos(angle) * radius;
+			line(frame, Point(x, y), Point(x+sin(angle)*15, y+cos(angle)*15), Scalar(255, 255, 255), 2);
+		}
+
+    	if (nrof_imgs < MAX_IMGS){
+    		mkdir(SAVE_DIR.c_str(), 0777);
+		    string dirName = SAVE_DIR + IDName + "/";
+		    mkdir(dirName.c_str(), 0777);
+            vector<struct Bbox> Boxes = detector.findFace(frame);
+            for(vector<struct Bbox>::iterator it=Boxes.begin(); it!=Boxes.end();it++){
+                if((*it).exist){
+                    Mat face = frame(Rect((*it).y1, (*it).x1, abs((*it).y1 - (*it).y2), abs((*it).x1 - (*it).x2)));
+                    rectangle(frame, Point((*it).y1, (*it).x1), Point((*it).y2, (*it).x2), Scalar(0,0,255), 2,8,0);
+                    putText(frame, "TURN HEAD AROUND!", Point(10, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255), 2, LINE_AA);
+                    if ((*it).score > 0.99) {
+                        string save_file = SAVE_DIR + IDName + "/" + to_string(nrof_imgs) + ".jpg";
+                        bool isSuccess = imwrite(save_file, face);
+                        if (isSuccess){
+                            cout << "Image saved at: " << save_file << endl;
+                            nrof_imgs++;
+                            break;
+                        }
+                        else{
+                            cerr << "ERROR: Failed to save the image \n";
+                        }
+                    }
+                }
+            }
+            Boxes.clear();
+        }
+        else if (nrof_imgs == MAX_IMGS){
+            putText(frame, "DONE! WAIT A SEC.", Point(frame.cols/2-50, frame.rows/2), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255), 2, LINE_AA);
+            //LOAD & TRAIN NEW SVMs
+            cout << "Added " << IDName << " Successfully!";
+            return -1;
+        }
+        imshow("result", frame);
+        start = clock() - start;
+        cout << "FPS: " << (int)(1/((double)start/CLOCKS_PER_SEC))<<endl;
+        if( waitKey(1)>=0 ) break;
+    }
+    cap.release();
+    destroyAllWindows();
+	return 0;
+}
--- src/main_predict.cpp
+++ src/main_predict.cpp
@@ -1,22 +1,16 @@
 #include <torch/script.h>
 #include <torch/torch.h>
-
 #include "mtcnn.h"
-
 #include <iostream>
 #include <ctime>
 #include <fstream>
-
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iomanip>
-
 #include "svm.h"
-
 
 using namespace cv;
 using namespace std;
-
 
 struct result{
 	double class_name;
@@ -33,56 +27,43 @@ int svm_type;
 int nr_class;
 
 
-void camera_test()
+int main()
 {
-
-	//////////////////////////////////////////////////DLIB/////////////////////////////////////////////////////////
-
-    if((model=svm_load_model("Data/test.model"))==0)
-    {
+    if((model=svm_load_model("Data/test.model"))==0){
         fprintf(stderr,"Can't open model file %s\n","test.model");
         exit(1);
     }
     svm_type=svm_get_svm_type(model);
-	nr_class=svm_get_nr_class(model);
+    nr_class=svm_get_nr_class(model);
     x = (struct svm_node *) malloc(513*sizeof(struct svm_node));
 
-	////////////////////////////////////////////////////TORCH////////////////////////////////////////////////////////
 
-	string model_path = "Data/model.pt";
+    string model_path = "Data/model.pt";
     module = torch::jit::load(model_path);
     cout << "Switch to GPU mode" << endl;
     module.to(at::kCUDA);
     vector<torch::jit::IValue> inputs;
 
-	/////////////////////////////////////////////////////Get Label///////////////////////////////////////////////////////////
-
-	fstream newfile;
-	vector<string> label;
-	newfile.open("Data/inverse_label.txt",ios::in);
-   	if (newfile.is_open()){  
-		string tp;
-		while(getline(newfile, tp))
-		{ 
- 			label.push_back(tp);
-		}
-		newfile.close();
-   	}
-
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    fstream newfile;
+    vector<string> label;
+    newfile.open("Data/inverse_label.txt",ios::in);
+    if (newfile.is_open()){  
+        string tp;
+        while(getline(newfile, tp)) label.push_back(tp);
+        newfile.close();
+    }
 
     Mat frame;
-    VideoCapture cap("http://192.168.1.4:4747/video");
+    VideoCapture cap(0);
     if(!cap.isOpened()){
         cout<<"Fail to open video!"<<endl;
-        return;
+        return -1;
     }
     clock_t start;
     cap>>frame;
     if(!frame.data) {
         cout << "unable to open camera" << endl;
-        return;
+        return -1;
     }
     mtcnn detector(frame.rows, frame.cols);
 
@@ -91,7 +72,7 @@ void camera_test()
         cap >> frame;
         if (frame.empty()) break;
         
-    	vector<struct Bbox> Boxes = detector.findFace(frame);
+        vector<struct Bbox> Boxes = detector.findFace(frame);
         for(vector<struct Bbox>::iterator it=Boxes.begin(); it!=Boxes.end();it++){
             if((*it).exist){
                 Mat face = frame(Rect((*it).y1, (*it).x1, abs((*it).y1 - (*it).y2), abs((*it).x1 - (*it).x2)));
@@ -99,12 +80,12 @@ void camera_test()
                 for(int num=0;num<5;num++)circle(frame,Point((int)*(it->ppoint+num), (int)*(it->ppoint+num+5)),3,Scalar(0,255,255), -1);
                 
                 result A = predict(face);
-            	ostringstream confidence_obj;
-            	confidence_obj << std::fixed;
-            	confidence_obj << std::setprecision(2);
-            	confidence_obj << A.confidence * 100;
-            	string conf = confidence_obj.str();
-            	string result_text = label[(int)(A.class_name)] + " " + conf;
+                ostringstream confidence_obj;
+                confidence_obj << std::fixed;
+                confidence_obj << std::setprecision(2);
+                confidence_obj << A.confidence * 100;
+                string conf = confidence_obj.str();
+                string result_text = label[(int)(A.class_name)] + " " + conf;
                 putText(frame, result_text.c_str(), Point((*it).y1, (*it).x1), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 255, 255), 1, LINE_AA);
             }
         }
@@ -116,12 +97,6 @@ void camera_test()
     }
     cap.release();
     destroyAllWindows();
-}
-
-
-int main()
-{
-    camera_test();
     return 1;
 }
 
@@ -129,46 +104,31 @@ result predict(Mat faces){
     if (faces.empty()){
         return {};
     }
-
     result A;
     double *prob_estimates=NULL;
-    // struct svm_node *x;
     double predict_label;
-
     vector<torch::jit::IValue> inputs;
-
     cv::resize(faces, faces, Size(112,112));
     faces.convertTo(faces, CV_32FC3, 1.0f / 255.0f);
 
     auto input_tensor = torch::from_blob(faces.data, {1, 112, 112, 3});
     input_tensor = input_tensor.permute({0, 3, 1, 2});
-
     input_tensor[0][0] = input_tensor[0][0].sub_(0.5).div_(0.5);
     input_tensor[0][1] = input_tensor[0][1].sub_(0.5).div_(0.5);
     input_tensor[0][2] = input_tensor[0][2].sub_(0.5).div_(0.5);
-
     input_tensor = input_tensor.to(at::kCUDA);
-
     torch::Tensor out_tensor = module.forward({input_tensor}).toTensor();
 
 
-    for(int i = 0; i < 512; i++)
-    {
+    for(int i = 0; i < 512; i++){
         x[i].index = i;
         x[i].value = out_tensor[0][i].item<double>();
-        // cout << x[i].index << ": " << x[i].value << endl;
     }
-
     x[512].index = -1;
-
-
     prob_estimates = (double *) malloc(nr_class*sizeof(double));
-
 	predict_label = svm_predict_probability(model,x,prob_estimates);
-
 
 	A.class_name = predict_label;
 	A.confidence = *(prob_estimates);
-
     return A;
 }
